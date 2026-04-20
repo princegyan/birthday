ed9bff265c29fbcccf328a1611b9eb358960320b
Alfred Prince Gyan <agyan@inlaks.com>
2026-04-20 23:58:55 +0000

    Initial commit - Birthday website for Manuella

---
+++ src/App.jsx
@@ -0,0 +1,349 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+//  import viteLogo from '/assets/1.jpg'
+// import heroImg from './assets/hero.png'
+import { Dialog, DialogPanel } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import './App.css'
+
+const navigation = [
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' },
+]
+
+function App() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  
+
+  return (
+    <>
+      <div className="bg-white">
+            <header className="absolute inset-x-0 top-0 z-50">
+              {/* <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+                <div className="flex lg:flex-1">
+                  <a href="#" className="-m-1.5 p-1.5">
+                    <span className="sr-only">Your Company</span>
+                    <img
+                      alt=""
+                      src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                      className="h-8 w-auto"
+                    />
+                  </a>
+                </div>
+                <div className="flex lg:hidden">
+                  <button
+                    type="button"
+                    onClick={() => setMobileMenuOpen(true)}
+                    className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                  >
+                    <span className="sr-only">Open main menu</span>
+                    <Bars3Icon aria-hidden="true" className="size-6" />
+                  </button>
+                </div>
+                <div className="hidden lg:flex lg:gap-x-12">
+                  {navigation.map((item) => (
+                    <a key={item.name} href={item.href} className="text-sm/6 font-semibold text-gray-900">
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                  <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                    Log in <span aria-hidden="true">&rarr;</span>
+                  </a>
+                </div>
+              </nav> */}
+              {/* <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+                <div className="fixed inset-0 z-50" />
+                <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                  <div className="flex items-center justify-between">
+                    <a href="#" className="-m-1.5 p-1.5">
+                      <span className="sr-only">Your Company</span>
+                      <img
+                        alt=""
+                        src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                        className="h-8 w-auto"
+                      />
+                    </a>
+                    <button
+                      type="button"
+                      onClick={() => setMobileMenuOpen(false)}
+                      className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                    >
+                      <span className="sr-only">Close menu</span>
+                      <XMarkIcon aria-hidden="true" className="size-6" />
+                    </button>
+                  </div>
+                  <div className="mt-6 flow-root">
+                    <div className="-my-6 divide-y divide-gray-500/10">
+                      <div className="space-y-2 py-6">
+                        {navigation.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                          >
+                            {item.name}
+                          </a>
+                        ))}
+                      </div>
+                      <div className="py-6">
+                        <a
+                          href="#"
+                          className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                        >
+                          Log in
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </DialogPanel>
+              </Dialog> */}
+            </header>
+            <main>
+              <div className="relative isolate">
+                <svg
+                  aria-hidden="true"
+                  className="absolute inset-x-0 top-0 -z-10 h-[64rem] w-full stroke-gray-200 [mask-image:radial-gradient(32rem_32rem_at_center,white,transparent)]"
+                >
+                  <defs>
+                    <pattern
+                      x="50%"
+                      y={-1}
+                      id="1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84"
+                      width={200}
+                      height={200}
+                      patternUnits="userSpaceOnUse"
+                    >
+                      <path d="M.5 200V.5H200" fill="none" />
+                    </pattern>
+                  </defs>
+                  <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
+                    <path
+                      d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
+                      strokeWidth={0}
+                    />
+                  </svg>
+                  <rect fill="url(#1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84)" width="100%" height="100%" strokeWidth={0} />
+                </svg>
+                <div
+                  aria-hidden="true"
+                  className="absolute top-0 right-0 left-1/2 -z-10 -ml-24 transform-gpu overflow-hidden blur-3xl lg:ml-24 xl:ml-48"
+                >
+                  <div
+                    style={{
+                      clipPath:
+                        'polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)',
+                    }}
+                    className="aspect-801/1036 w-[50.0625rem] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+                  />
+                </div>
+                <div className="overflow-hidden">
+                  <div className="mx-auto max-w-7xl px-6 pt-36 pb-32 sm:pt-60 lg:px-8 lg:pt-32">
+                    <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
+                      <div className="relative w-full lg:max-w-xl lg:shrink-0 xl:max-w-2xl">
+                        <h1 className="font-Guttery text-5xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-7xl">
+                          Happy Birthday, Manuella! <span className="text-4xl text-gray-500">🎉</span>
+                        </h1>
+                        <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:max-w-md sm:text-xl/8 lg:max-w-none">To the one who makes everything feel beautiful
+I saw your picture on a friend's status and I couldn't stop myself from reaching out. You have such a beautiful, warm smile — it honestly made my day.
+
+I know this is a bit random and we've never met, but I wanted to wish you a very happy birthday anyway. Hope you're having a wonderful day surrounded by people who make you smile as much as that photo made me smile.
+
+Take care 😊
+                        </p>
+                        {/* <div className="mt-10 flex items-center gap-x-6">
+                          <a
+                            href="#"
+                            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                          >
+                            Get started
+                          </a>
+                          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                            Live demo <span aria-hidden="true">→</span>
+                          </a>
+                        </div> */}
+                      </div>
+                      
+                      <div className="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
+                        <div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
+                          <div className="relative">
+                            <img
+                              alt=""
+                              src="./1.jpg"
+                              className="aspect-2/3 w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                            />
+                            <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-gray-900/10 ring-inset" />
+                          </div>
+                        </div>
+                        <div className="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
+                          <div className="relative">
+                            <img
+                              alt=""
+                              src="./2.jpg"
+                              className="aspect-2/3 w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                            />
+                            <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-gray-900/10 ring-inset" />
+                          </div>
+                          <div className="relative">
+                            <img
+                              alt=""
+                              src="./3.jpg"
+                              className="aspect-2/3 w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                            />
+                            <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-gray-900/10 ring-inset" />
+                          </div>
+                        </div>
+                        <div className="w-44 flex-none space-y-8 pt-32 sm:pt-0">
+                          <div className="relative">
+                            <img
+                              alt=""
+                              src="./4.jpg"
+                              className="aspect-2/3 w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                            />
+                            <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-gray-900/10 ring-inset" />
+                          </div>
+                          <div className="relative">
+                            <img
+                              alt=""
+                              src="./5.jpg"
+                              className="aspect-2/3 w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                            />
+                            <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-gray-900/10 ring-inset" />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </main>
+          </div>
+        <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
+        {/* <h2 className="text-center text-base/7 font-semibold text-indigo-600">Deploy faster</h2> */}
+        <p className="mx-auto mt-2 max-w-lg text-center text-4xl font-semibold tracking-tight text-balance text-gray-950 sm:text-5xl">
+          Caught Your Smile on My Screen And Now I Can't Stop Smiling
+        </p>
+        <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
+          <div className="relative lg:row-span-2">
+            
+            <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
+              <img
+              className="size-full object-cover object-top"
+              src="./11.jpeg"
+              alt=""
+            />
+              {/* <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
+                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
+                  Picture 1 
+                </p>
+                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
+                </p>
+              </div> */}
+              {/* <div className="@container relative min-h-[30rem] w-full grow max-lg:mx-auto max-lg:max-w-sm">
+                <div className="absolute inset-x-10 top-10 bottom-0 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 shadow-2xl">
+                  <img
+                    className="size-full object-cover object-top"
+                    src="https://tailwindui.com/plus-assets/img/component-images/bento-03-mobile-friendly.png"
+                    alt=""
+                  />
+                </div>
+              </div> */}
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 lg:rounded-l-[2rem]"></div>
+          </div>
+          <div className="relative max-lg:row-start-1">
+            <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
+              {/* <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Performance</p>
+                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit maiores impedit.
+                </p>
+              </div> */}
+              {/* <div className="flex flex-1 items-center justify-center px-8 max-lg:pt-10 max-lg:pb-12 sm:px-10 lg:pb-2">
+                <img
+                  className="w-full max-lg:max-w-xs"
+                  src="https://tailwindui.com/plus-assets/img/component-images/bento-03-performance.png"
+                  alt=""
+                />
+              </div> */}
+              <img
+              className="size-full object-cover object-top"
+              src="./9.jpeg"
+              alt=""
+            />
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-t-[2rem]"></div>
+          </div>
+          <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
+            <div className="absolute inset-px rounded-lg bg-white"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)]">
+              {/* <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Security</p>
+                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                  Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi.
+                </p>
+              </div> */}
+              {/* <div className="@container flex flex-1 items-center max-lg:py-6 lg:pb-2">
+                <img
+                  className="h-[min(152px,40cqw)] object-cover"
+                  src="https://tailwindui.com/plus-assets/img/component-images/bento-03-security.png"
+                  alt=""
+                />
+              </div> */}
+              <img
+              className="size-full object-cover object-top"
+              src="./8.jpeg"
+              alt=""
+            />
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5"></div>
+          </div>
+          <div className="relative lg:row-span-2">
+            <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
+              {/* <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
+                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
+                  Powerful APIs
+                </p>
+                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                  Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget sem sodales gravida.
+                </p>
+              </div> */}
+              <div className="relative min-h-[30rem] w-full grow">
+                 <img
+              className="size-full object-cover object-top"
+              src="./7.jpg"
+              alt=""
+            />
+              </div>
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+          </div>
+        </div>
+      </div>
+          <footer className="bg-white">
+      <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
+        <div className="flex justify-center gap-x-6 md:order-2">
+          {/* {navigation.map((item) => (
+            <a key={item.name} href={item.href} className="text-gray-600 hover:text-gray-800">
+              <span className="sr-only">{item.name}</span>
+              <item.icon aria-hidden="true" className="size-6" />
+            </a>
+          ))} */}
+        </div>
+        <p className="mt-8 text-center text-sm/6 text-gray-600 md:order-1 md:mt-0">
+          &copy; Made with ❤️ Prince Alfred.
+        </p>
+      </div>
+    </footer>
+    </>
+  )
+}
+
+export default App
