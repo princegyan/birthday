087a6985881225957b9c91cc41d3187c7299dca3
Alfred Prince Gyan <agyan@inlaks.com>
2026-04-21 00:36:39 +0000

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -224,7 +224,7 @@ Take care 😊
         <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
         {/* <h2 className="text-center text-base/7 font-semibold text-indigo-600">Deploy faster</h2> */}
         <p className="mx-auto mt-2 max-w-lg text-center text-4xl font-semibold tracking-tight text-balance text-gray-950 sm:text-5xl">
-          Caught  Your  Smile  on  My  Screen  And  Now  I  Can't  Stop  Smiling
+          Caught  Your  Smile  on  My  Screen  And  Now  I  Can't   Stop  Smiling
         </p>
         <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
           <div className="relative lg:row-span-2">
